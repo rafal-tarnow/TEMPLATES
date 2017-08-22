9c8353539bb0872f71a3fbed319b981896bc28f8
Rafal Kruszyna <rafal.kruszyna@zmt.tarnow.pl>
2017-08-22 15:41:16 +0200

    aktualizacjia przykładów

--- OPEN_GL_ES/Template_OpenGL_ES_2/main.cpp
+++ OPEN_GL_ES/Template_OpenGL_ES_2/main.cpp
@@ -4,6 +4,10 @@
 #define GLFW_INCLUDE_ES2
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+using namespace std;
+
 #include "texture_manager.hpp"
 
 static const GLuint WIDTH = 800;
@@ -12,7 +16,7 @@ void InitGLFWWindow();
 GLint compileShaders(const char *vertex_shader_source, const char *fragment_shader_source);
 void initOpenGLViewport();
 GLuint prepareVBO(const GLfloat * data, GLsizeiptr size);
-
+void drawTexturedTriangle();
 
 
 
@@ -31,18 +35,12 @@ static const GLchar* fragment_shader_source =
         "#version 100                               \n"
         "precision mediump float;                   \n"
         "varying vec2 v_TexCoordinate;              \n"
-        "uniform sampler2D ourTexture;              \n"
+        "uniform sampler2D textureUnit;              \n"
         "void main() {                              \n"
-        "   gl_FragColor = texture2D(ourTexture,v_TexCoordinate);\n"
+        "   gl_FragColor = texture2D(textureUnit,v_TexCoordinate);\n"
         "}                                          \n";
 
 
-//static const GLfloat quad_vertices[] = {
-//    0.5f,  0.5f, 0.0f,
-//    0.5f, -0.5f, 0.0f,
-//    -0.5f, -0.5f, 0.0f,
-//    -0.5f, 0.5f, 0.0f,
-//};
 
 static const GLfloat quad_vertices[] = {
     0.5f,  0.5f,    1.0f,1.0f,
@@ -57,36 +55,25 @@ static const GLfloat triangle_vertices[] = {
     -0.5f, -0.5f, 0.0f,     0.0f, 0.0f
 };
 
-
+static const GLfloat second_triangle_vertices[] = {
+    0.5f,  0.5f, 0.0f,      1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f,      1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f,     0.0f, 0.0f
+};
 
 GLFWwindow* window;
 GLuint shader_program;
 GLuint triangle_vbo_id;
+GLuint second_triangle_vbo_id;
 GLuint rectangle_vbo_id;
 GLint position_location;
 GLint texCoord_attrib_location;
-GLuint texture_id;
-
-void renderFunction(){
-    glClear(GL_COLOR_BUFFER_BIT );
-
-    glUseProgram(shader_program);
+GLint textureUnitLocation;;
+GLuint bg_texture_id;
+GLuint coin_texture_id;
 
 
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, texture_id);
-//        glUniform1i(texCoord_attrib_location, 0);
-//        glBindBuffer(GL_ARRAY_BUFFER, rectangle_vbo_id);
-//        {
-
-//            glVertexAttribPointer(position_location, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
-//            glEnableVertexAttribArray(position_location);
-//            glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-//             //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-//        }
-//        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-
+void drawTexturedTriangle(){
     glBindBuffer(GL_ARRAY_BUFFER, triangle_vbo_id);
 
 
@@ -97,12 +84,51 @@ void renderFunction(){
     glEnableVertexAttribArray(texCoord_attrib_location);
 
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glBindTexture(GL_TEXTURE_2D, coin_texture_id);
     glUniform1i(texCoord_attrib_location, 0);
 
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
+void drawTexturedSecondTriangle(){
+    glBindBuffer(GL_ARRAY_BUFFER, second_triangle_vbo_id);
+
+    glVertexAttribPointer(position_location, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(position_location);
+
+    glVertexAttribPointer(texCoord_attrib_location, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(texCoord_attrib_location);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, bg_texture_id);
+    glUniform1i(textureUnitLocation, GL_TEXTURE0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void renderFunction(){
+    glClear(GL_COLOR_BUFFER_BIT );
+
+    glUseProgram(shader_program);
+
+
+    //        glActiveTexture(GL_TEXTURE0);
+    //        glBindTexture(GL_TEXTURE_2D, texture_id);
+    //        glUniform1i(texCoord_attrib_location, 0);
+    //        glBindBuffer(GL_ARRAY_BUFFER, rectangle_vbo_id);
+    //        {
+
+    //            glVertexAttribPointer(position_location, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)0);
+    //            glEnableVertexAttribArray(position_location);
+    //            glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+    //             //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    //        }
+    //        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //drawTexturedTriangle();
+    drawTexturedSecondTriangle();
 
     glUseProgram(0);
 
@@ -114,19 +140,23 @@ int main(void) {
     InitGLFWWindow();
     initOpenGLViewport();
 
-
+    GLint i;
+    glGetIntegerv(GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, &i);
+    cout << "Max texture units = " << int(i) << endl;
 
     shader_program = compileShaders(vertex_shader_source, fragment_shader_source);
 
     position_location = glGetAttribLocation(shader_program, "position");
     texCoord_attrib_location = glGetAttribLocation(shader_program,"texCoord");
+    textureUnitLocation = glGetUniformLocation (shader_program, "textureUnit" );
 
     //rectangle
     rectangle_vbo_id = prepareVBO(quad_vertices, sizeof(quad_vertices));
     triangle_vbo_id = prepareVBO(triangle_vertices, sizeof(triangle_vertices));
+    second_triangle_vbo_id = prepareVBO(second_triangle_vertices, sizeof(second_triangle_vertices));
 
-    texture_id = TextureManager::getTextureId("./data/png/bg.png");
-    //texture_id = TextureManager::getTextureId("./data/png/coin_2.png");
+    bg_texture_id = TextureManager::getTextureId("./data/png/bg.png");
+    coin_texture_id = TextureManager::getTextureId("./data/png/coin_2.png");
 
 
 
