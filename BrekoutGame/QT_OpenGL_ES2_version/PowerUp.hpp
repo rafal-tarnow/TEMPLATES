8fc8dfdd3f845671aed291a14710f126c87d8e82
Rafal Kruszyna <rafal.kruszyna@zmt.tarnow.pl>
2017-09-07 11:14:14 +0200

    dorobienie Brekout na ES2

---
+++ BrekoutGame/QT_OpenGL_ES2_version/PowerUp.hpp
@@ -0,0 +1,48 @@
+//
+//  PowerUp.hpp
+//  OpenGLBreakout
+//
+//  Created by 梅宇宸 on 16/12/26.
+//  Copyright © 2016年 梅宇宸. All rights reserved.
+//
+
+#ifndef PowerUp_hpp
+#define PowerUp_hpp
+
+#include <string>
+
+#include "opengl_includes.hpp"
+#include <glm/glm.hpp>
+
+#include "GameObject.hpp"
+
+const glm::vec2 SIZE (60, 20);
+const glm::vec2 VELOCITY (0.0f, 150.0f);
+
+const glm::vec3 ORANGE_RED (1.0f, 0.27f, 0.0f);
+const glm::vec3 LAWN_GREEN (0.5f, 0.96f, 0.0f);
+const glm::vec3 YELLOW (1.0f, 1.0f, 1.0f);
+const glm::vec3 DODGER_BLUE (0.11f, 0.56f, 1.0f);
+const glm::vec3 PURPLE (0.51f, 0.0f, 0.51f);
+const glm::vec3 PINK (1.0f, 20 / 255, 147 / 255);
+const glm::vec3 CHOCOLATE (210 / 255, 105 / 255, 30 / 255);
+const glm::vec3 GRAY (0.5f, 0.5f, 0.5f);
+
+class PowerUp : public GameObject
+{
+public:
+    // PowerUp State
+    std::string Type;
+    GLfloat Duration;
+    GLboolean Activated;
+    
+    // Constructor
+    PowerUp (std::string type, glm::vec3 color, GLfloat duration,
+             glm::vec2 position, Texture2D texture)
+    : GameObject (position, SIZE, texture, color, VELOCITY), Type(type), Duration(duration), Activated()
+    {
+        
+    }
+};
+
+#endif /* PowerUp_hpp */
---
+++ BrekoutGame/QT_OpenGL_ES2_version/opengl_includes.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+
+//#include <GL/glew.h>
+
+#define GLFW_INCLUDE_ES3
+#include <GLFW/glfw3.h>
